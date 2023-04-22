04fda324f48b251fd3544b8159f6d21834a169e7
Filip Vrba <filipvrba@pm.me>
2023-04-22 11:32:25 +0200

    update footer

--- src/.js/elements/elm_footer.js
+++ src/.js/elements/elm_footer.js
@@ -7,8 +7,7 @@ export default class ElmFooter extends HTMLElement {
   init_html() {
     let template = `${`
       <footer class='py-3 my-4'>
-          <span class='nav justify-content-center border-bottom pb-3 mb-3'></span>
-          <p class='text-center text-body-secondary'>© 2023 Prototype for DragonRuby</p>
+          <p class='text-center text-body-secondary my-1'>© 2023 Prototype for DragonRuby</p>
           <p class='text-center text-body-secondary'>
           Created by <a href='https://www.reddit.com/user/Intelligent-End-9399'>
             Intelligent-End-9399</a>
