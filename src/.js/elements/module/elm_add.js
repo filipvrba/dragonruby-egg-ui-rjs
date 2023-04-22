233b04cdcfd2d1a03b3f4c1a2ad76e47fd5848ae
Filip Vrba <filipvrba@pm.me>
2023-04-22 08:51:45 +0200

    add regex for github_url

--- src/.js/elements/module/elm_add.js
+++ src/.js/elements/module/elm_add.js
@@ -25,6 +25,7 @@ export default class ElmModuleAdd extends HTMLElement {
       <div class='input-group input-group-sm mb-3'>
         <span class='input-group-text' id='inputGroup-sizing-sm'>GitHub URL</span>
         <input id='github_url' type='text' class='form-control' oninput='change_module_add()' aria-label='GitHub URL' aria-describedby='inputGroup-sizing-sm'>
+        <div class='invalid-feedback'>Invalid page for GitHub repository.</div>
       </div>
       <div class='input-group input-group-sm mb-3'>
         <span class='input-group-text'>Description</span>
@@ -39,21 +40,25 @@ export default class ElmModuleAdd extends HTMLElement {
 
   click_module_add() {
     if (confirm("Do you really want to add this module?")) {
-      database.query(
-        `SELECT name FROM modules WHERE name='${this._module_name.value}'`,
+      if (this._github_url.value.match(ElmModuleAdd.GITHUB_REGEX)) {
+        database.query(
+          `SELECT name FROM modules WHERE name='${this._module_name.value}'`,
 
-        (data) => {
-          if (data.length == 0) {
-            let inset_query = `INSERT INTO modules (author, name, github_url, description, created_at) VALUES ('none', '${this._module_name.value}', '${this._github_url.value}', '${this._description.value}', ${Time.unix()})`;
+          (data) => {
+            if (data.length == 0) {
+              let inset_query = `INSERT INTO modules (author, name, github_url, description, created_at) VALUES ('none', '${this._module_name.value}', '${this._github_url.value}', '${this._description.value}', ${Time.unix()})`;
 
-            database.query(inset_query, (is_ok) => {
-              if (is_ok) change_page("home")
-            })
-          } else {
-            this._module_name.classList.add("is-invalid")
+              database.query(inset_query, (is_ok) => {
+                if (is_ok) change_page("home")
+              })
+            } else {
+              this._module_name.classList.add("is-invalid")
+            }
           }
-        }
-      )
+        )
+      } else {
+        this._github_url.classList.add("is-invalid")
+      }
     }
   };
 
@@ -68,6 +73,12 @@ export default class ElmModuleAdd extends HTMLElement {
 
     if (this._module_name.classList.value.indexOf("is-invalid") > -1) {
       this._module_name.classList.remove("is-invalid")
+    };
+
+    if (this._github_url.classList.value.indexOf("is-invalid") > -1) {
+      this._github_url.classList.remove("is-invalid")
     }
   }
-}
+};
+
+ElmModuleAdd.GITHUB_REGEX = /^https:\/\/github.com\/([a-zA-Z0-9\-\_]{1,255})\/[a-zA-Z0-9\-\_]{1,100}(.git)?$/m
