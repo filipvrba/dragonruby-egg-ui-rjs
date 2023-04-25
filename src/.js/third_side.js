b5275ada039e81d32fe1eb32723173b6d63d25d3
Filip Vrba <filipvrba@pm.me>
2023-04-25 10:20:46 +0200

    remove lol fn

--- src/.js/third_side.js
+++ src/.js/third_side.js
@@ -3,5 +3,4 @@ import Authorization from "./third-side/bef/authorization";
 import Time from "./third-side/time";
 window.database = new Database("1");
 window.authorization = new Authorization;
-window.Time = Time;
-function lol() {}
+window.Time = Time
